6ce8ed34b2f29f9354100fe6b73c8900cefda4a4
Minh Pham <mpham@naver.com>
2019-01-23 23:27:10 +0100

    TDD of city model validations

--- test/models/city_test.rb
+++ test/models/city_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class CityTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not save if name is not present" do
+    city = City.new
+    assert_not city.save
+  end
+
+  test "should not save if name is not unique" do
+    city = City.create(name: "Berlin")
+    assert_not city.save
+  end
+
 end
